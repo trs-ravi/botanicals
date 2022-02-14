c1bea17e3c2b6ef09e4dd6fbfa329728ba9f698e
Lalit singh <lalit01trs@gmail.com>
2022-02-14 10:49:23 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/* document.getElementById('dropdownMenuButton').addEventListener(
+  'click',
+  function() {
+	this.classList.toggle('menu-toggle-active');
+  }
+); */
+AOS.init({
+  duration: 1200,
+});
